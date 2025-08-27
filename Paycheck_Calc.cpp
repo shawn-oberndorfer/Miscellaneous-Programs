4895400a2a031b22f6d84d48fe5aa0cf7670bd80
Shawn Oberndorfer <obesr2008@icloud.com>
2025-08-27 18:49:24 +0000

    Fixed format and and added the ability to restart the program.

--- Paycheck_Calc.cpp
+++ Paycheck_Calc.cpp
@@ -11,6 +11,7 @@ int main(){
     string user_name      = "";
     char   wage_confirm   = 'N';
     char   hours_confirm  = 'N';
+    char   run_again      = 'N';
     double hours_worked   = 0;
     double hourly_wage    = 0;
     double total_salary   = 0;
@@ -19,52 +20,63 @@ int main(){
 
     cout << "Hello what is your name?" << endl;
     cin >> user_name; 
-    cout << "Welcome " << user_name << " to paycheck calculator." << endl;
-    cout << "Please enter what your hourly rate is." << endl;
+
+    do {
+
+        cout << "Welcome " << user_name << " to paycheck calculator." << endl;
+        cout << "Please enter what your hourly rate is." << endl;
     
-    do {
-    cin >> hourly_wage;
-    cout << "To confirm, do you make $" << hourly_wage << " an hour. (Y/N)" << endl;
-    cin >> wage_confirm;
-    wage_confirm = toupper(wage_confirm);
+        do {
+            cin >> hourly_wage;
+            cout << "To confirm, do you make $" << hourly_wage << " an hour. (Y/N)" << endl;
+            cin >> wage_confirm;
+            wage_confirm = toupper(wage_confirm);
 
-    if(wage_confirm == 'Y') {
-        cout << "Hourly Wage Confirmed" << endl;
-    }   else if(wage_confirm == 'N') {
-        cout << "Please re-enter your hourly wage." << endl;
-    }   else {
-        cout << "Invalid Input" << endl;
-        cout << "Please re-enter your hourly wage followed by a \"Y\" or a \"N\"." << endl;
-        wage_confirm = 'N';
+            if(wage_confirm == 'Y') {
+                cout << "Hourly Wage Confirmed" << endl;
+
+        }   else if(wage_confirm == 'N') {
+                cout << "Please re-enter your hourly wage." << endl;
+                
+        }   else {
+                cout << "Invalid Input" << endl;
+                cout << "Please re-enter your hourly wage followed by a \"Y\" or a \"N\"." << endl;
+                wage_confirm = 'N';
     }
-    } while (wage_confirm == 'N');
+    }   while (wage_confirm == 'N');
 
-    cout << "How many hours did you work this week?" << endl;
+        cout << "How many hours did you work this week?" << endl;
 
     do {
-    cin >> hours_worked;
-    cout << "To confirm, did you work " << hours_worked << " hours this week. (Y/N)" << endl;
-    cin >> hours_confirm;
-    hours_confirm = toupper(hours_confirm);
+        cin >> hours_worked;
+        cout << "To confirm, did you work " << hours_worked << " hours this week. (Y/N)" << endl;
+        cin >> hours_confirm;
+        hours_confirm = toupper(hours_confirm);
 
-    if(hours_confirm == 'Y') {
-        cout << "Hours Worked Confirmed" << endl;
+        if(hours_confirm == 'Y') {
+            cout << "Hours Worked Confirmed" << endl;
+
     }   else if(hours_confirm == 'N') {
-        cout << "Please re-enter your hours worked." << endl;
+            cout << "Please re-enter your hours worked." << endl;
+
     }   else {
-        cout << "Invalid Input" << endl;
-        cout << "Please re-enter your hours worked followed by a \"Y\" or a \"N\"." << endl;
-        hours_confirm = 'N';
+            cout << "Invalid Input" << endl;
+            cout << "Please re-enter your hours worked followed by a \"Y\" or a \"N\"." << endl;
+            hours_confirm = 'N';
     }
-    } while (hours_confirm == 'N');
+    }   while (hours_confirm == 'N');
 
 
-    total_salary = hours_worked * hourly_wage;
-    cout << endl;
-    cout << "Your paycheck this week would be $" << total_salary << "." << endl;
-    cout << endl;
-    cout << "Would you like to run the program again?" << endl;
+        total_salary = hours_worked * hourly_wage;
+        cout << endl;
+        cout << "Your paycheck this week would be $" << total_salary << "." << endl;
+        cout << endl;
+        cout << "If you would like to run the program again enter \"Y\". If not enter any character." << endl;
+        cin >> run_again;
 
+    } while (run_again == 'Y');
+
+    cout << "Goodbye!" << endl;
 
 return 0;
 }
