62c3a54f2a40683dfb871a925a0055931c32428f
Shawn Oberndorfer <obesr2008@icloud.com>
2025-09-22 23:40:01 +0000

    Added full game functionality and two bet options.

--- Craps.cpp
+++ Craps.cpp
@@ -1,33 +1,54 @@
 /* 
-M3T2 - Craps Part 1
-CSC 134
 Shawn Oberndorfer
+Craps Simulator
 Start Date 9/22/2025
 Last Worked On 9/22/2025
 */
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <limits>
 
 using namespace std;
 
 int roll();
+int moneyTotal();
+int preRollBet();
 int firstRollResults();
+int pointRollResults();
+int winCondition();
+int gameOver();
 
 int roll1;
 int roll2;
 int point;
+int betNumber;
+int sum;
+int winConditionNum;
+int winConditionCheck;
+double betAmount;
+double totalMoney;
 bool gameEnd;
+bool moneyConfirm;
+string blankSpace;
+char moneyConfirm2;
 
 int main()
 {
+   srand(time(0));
+   moneyTotal();
+   preRollBet();
+   firstRollResults();
    
-    srand(time(0));
-
-    
-    
-
-
+   if(gameEnd == true){
+    winCondition();
+    gameOver();
+   }
+   else{
+    pointRollResults();
+    winCondition();
+    gameOver();
+   }
     return 0;
 }
 
@@ -36,24 +57,100 @@ int roll(){
     myRoll = (rand() % 6) + 1;
     return myRoll;
 }
+int moneyTotal(){
+    do{
+        cout << "How much money do you have?" << endl;
+        cin >> totalMoney;
+        cout << "Confirm you have $" << totalMoney << ". (Y/N)" << endl;
+        cin >> moneyConfirm2;
+        moneyConfirm2 = toupper(moneyConfirm2);
+        if(moneyConfirm2 == 'Y'){
+            moneyConfirm = true;
+        } else{
+            moneyConfirm = false;
+        }
+    }while(moneyConfirm == false);
+    return 0;
+}
+int preRollBet(){
+    cout << "What bet would you like to place?" << endl;
+    cout << "1. Pass Line" << endl;
+    cout << "2. Don't Pass Line" << endl;
+    cin >> betNumber;
+    if(betNumber == 1){
+        winConditionNum = 1;
+    } 
+    else if(betNumber == 2){
+        winConditionNum = 2;
+    }
+    cout << "How much money would you like to bet?" << endl;
+    cin >> betAmount;
+    return 0;
+}
 int firstRollResults(){
     roll1 = roll();
     roll2 = roll();
-    int sum = roll1 + roll2;
+    sum = roll1 + roll2;
 
     cout << "ROLL: " << sum << endl;
 
     if (sum == 7 || sum == 11){
-        cout <<"You Win!" << endl;
+        winConditionCheck = 1;
         gameEnd = true;
     }
     else if(sum == 2 || sum == 3 || sum == 12){
-        cout << "You Lose" << endl;
+        winConditionCheck = 2;
         gameEnd = true;
     } 
     else if(sum == 4 || sum == 5 || sum == 6 || sum == 8 || sum == 9 || sum == 10){
-            point = sum;
-            cout << "Your point is: " << point << endl;
-            gameEnd = false;
+        point = sum;
+        cout << "The point is: " << point << endl;
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        cout << "Hit 'enter' to roll again." << endl;
+        cin.get();
+        gameEnd = false;
     }
+
+    return 0;
+}
+int pointRollResults(){
+    do{
+        roll1 = roll();
+        roll2 = roll();
+        sum = roll1 + roll2;
+
+        cout << "ROLL: " << sum << endl;
+        if(sum == point){
+            winConditionCheck = 1;
+            gameEnd = true;
+        } else if(sum == 7){
+            winConditionCheck = 2;
+            gameEnd = true;
+        }else{
+            cout << "Roll again." << endl;
+            cin.ignore();
+            gameEnd = false;
+        }
+
+    }while(gameEnd == false);
+
+    return 0;
+}
+int winCondition(){
+    if(winConditionNum == winConditionCheck){
+        cout << "Congrats You Win!" << endl;
+        totalMoney = totalMoney + betAmount;
+        cout << "You now have $" << totalMoney << "." << endl;
+    }
+    else{
+        cout << "Sorry you lose." << endl;
+        totalMoney = totalMoney - betAmount;
+        cout << "You now have $" << totalMoney << "." << endl;
+    }
+    return 0;
+}
+int gameOver(){
+    cout << "------Game Over------" << endl;
+    cout << "Thank You For Playing" << endl;
+    return 0;
 }
